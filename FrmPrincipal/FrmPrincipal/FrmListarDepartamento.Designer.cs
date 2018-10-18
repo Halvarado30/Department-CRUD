4b4287da8eb1e17b5b8a48a6cb9e4dcaf8d37a6b
h.alvarado3098 <h.alvarado3098@hotmail.com>
2018-10-18 07:14:44 -0600

    Actualización del formulario ListarDepartamento

--- FrmPrincipal/FrmPrincipal/FrmListarDepartamento.Designer.cs
+++ FrmPrincipal/FrmPrincipal/FrmListarDepartamento.Designer.cs
@@ -28,32 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lstbListarDepto = new System.Windows.Forms.ListBox();
+            this.btnRegresar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lstbListarDepto
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(41, 27);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(601, 388);
-            this.listBox1.TabIndex = 0;
+            this.lstbListarDepto.FormattingEnabled = true;
+            this.lstbListarDepto.ItemHeight = 16;
+            this.lstbListarDepto.Location = new System.Drawing.Point(41, 27);
+            this.lstbListarDepto.Name = "lstbListarDepto";
+            this.lstbListarDepto.Size = new System.Drawing.Size(601, 388);
+            this.lstbListarDepto.TabIndex = 0;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.Location = new System.Drawing.Point(484, 431);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(158, 44);
+            this.btnRegresar.TabIndex = 1;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
             // 
             // FrmListarDepartamento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 498);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(704, 498);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.lstbListarDepto);
             this.Name = "FrmListarDepartamento";
-            this.Text = "FrmListarDepartamento";
+            this.Text = "Listar Departamento";
+            this.Load += new System.EventHandler(this.FrmListarDepartamento_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lstbListarDepto;
+        private System.Windows.Forms.Button btnRegresar;
     }
 }
--- FrmPrincipal/FrmPrincipal/FrmListarDepartamento.cs
+++ FrmPrincipal/FrmPrincipal/FrmListarDepartamento.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace FrmPrincipal
 {
     public partial class FrmListarDepartamento : Form
@@ -15,6 +17,50 @@ namespace FrmPrincipal
         public FrmListarDepartamento()
         {
             InitializeComponent();
+        }
+
+        private void FrmListarDepartamento_Load(object sender, EventArgs e)
+        {
+            // Establecer Conexión
+            SqlConnection conn = new SqlConnection(@"server = (local)\sqlexpress;
+                                    integrated security = true; database = AdventureWorks2014;");
+            // Creación del Query
+            string sql = "SELECT * FROM HumanResources.Department;";
+
+            // Creación del comando
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            try
+            {
+                // Abrimos la conexión
+                conn.Open();
+
+                // Ejecutamos el query mediante Datareader
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                //Llenamos con los datos
+                while (rdr.Read())
+                {
+                    lstbListarDepto.Items.Add(rdr[0]);
+                    lstbListarDepto.Items.Add(rdr[1]);
+                    lstbListarDepto.Items.Add(rdr[2]);
+                    lstbListarDepto.Items.Add(rdr[3]);
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace + "Detalles de la excepción");
+            }
+            finally
+            {
+                // Cerrar la conexión
+                conn.Close();
+            }
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
